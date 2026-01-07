251f24b0f105bd7d7ba66c89eedd60e37e1b0f44
aviiissh <avish2314@gmail.com>
2026-01-07 14:24:05 +0530

    some changes

--- new_source_ingestion.py
+++ new_source_ingestion.py
@@ -1,5 +1,12 @@
 source = 'new source'
 
 def new_source_cleanup(df_new):
-  logic here
+ # logic here
 
+  return df_new
+
+
+def date_logic_transformation(df_new[datecol]):
+  #some logic to change the date type
+
+  return df_new[col]
